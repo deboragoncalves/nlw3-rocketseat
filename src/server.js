a5451aea89f91fda9f8409b6ac09a54b47ef4fd0
Débora <deboracristinapereira123@gmail.com>
2020-11-21 13:07:08 -0300

    Pagina inicial servidor - parte 1 - aula 4

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
-// importar dependência (express). 
+// importar dependência (express e path). 
 // require: retorna uma função
 
 const express = require('express')
+const path = require('path')
 
 // iniciar express
 // retorna um objeto
 
 const server = express()
 
+// criar as rotas para as pastas que estão no public e são estáticas
+
+server.use(express.static('public'))
+
+// Configurar template engine
+
+// Buscar arquivos da views e definir hbs como engine de visualizacao
+// __dirname: localização atual. Join: une todos os parâmetros e retorna C/users/.../nlw3/src/views/index.html
+
+server.set('views', path.join(__dirname, "views")).set('view engine', 'hbs')
+
 // criar caminho (default: / = /index.html)
 // Dois parâmetros: request e response
 
-server.get('/', (request, response) => { response.send('Oi') })
+server.get('/', (request, response) => { 
+
+    response.render('index')
+
+})
 
 // ligar o servidor, que está escutando uma porta (5500)
 
